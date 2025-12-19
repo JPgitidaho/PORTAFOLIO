1a32f4d891fd86f2ba70eb294835d07a413d1a77
JPgitidaho <juanichile2015@gmail.com>
2025-12-19 17:33:01 -0300

    ACTUALIZATION

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -33,9 +33,9 @@ export default function Sidebar() {
           <span className="hidden md:inline">Proyectos</span>
         </NavLink>
 
-        {/* PERFIL PROFESIONAL */}
+       
         <NavLink
-          to="/profile"
+          to="/Profile"
           className={({ isActive }) =>
             `flex items-center gap-2 py-2 px-4 rounded-lg transition-all duration-300 ${
               isActive ? 'bg-cyan-600 text-white' : 'hover:bg-cyan-500/40'
