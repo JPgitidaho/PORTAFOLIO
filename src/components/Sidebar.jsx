fefdea0356e401102559c218ac22413efe26369d
JPgitidaho <juanichile2015@gmail.com>
2025-12-19 18:54:45 -0300

    actualization  final

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,11 @@
 import { NavLink } from 'react-router-dom'
-import { FaHome, FaFolderOpen, FaUser, FaTools, FaEnvelope } from 'react-icons/fa'
+import { FaHome, FaFolderOpen, FaUser, FaTools } from 'react-icons/fa'
 
 export default function Sidebar() {
   return (
     <aside className="fixed top-0 left-0 h-screen w-20 md:w-48 bg-white/10 backdrop-blur-lg border-r border-white/20 shadow-lg z-50 flex flex-col items-center py-6 space-y-6 text-white">
       
-      <div className="text-2xl font-bold tracking-wide text-cyan-400">JPxxxxx</div>
+      <div className="text-2xl font-bold tracking-wide text-cyan-400">JP</div>
 
       <nav className="flex flex-col space-y-4 mt-10 w-full px-2">
         
